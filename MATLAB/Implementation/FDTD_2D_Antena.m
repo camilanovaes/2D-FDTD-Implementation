c920d61f36f9000dec9198def63162f3e19d2967
Gustavo Moraes Vasconcelos <gmv_facul@hotmail.com>
2018-12-20 02:26:08 -0200

    ajeitando nomenclaturas

--- MATLAB/Implementation/FDTD_2D_Antena.m
+++ MATLAB/Implementation/FDTD_2D_Antena.m
@@ -24,7 +24,7 @@ deltat = (S/(c*sqrt(1/(deltax.^2)+1/(deltay.^2))));     % Temporal grid step obt
 
 % Gaussian font parameters
 T = 3*(time_tot*deltat/10);
-std_dev = 7.005203380146562e-11;
+sd = 7.005203380146562e-11;
 
 % Initialization of horn antenna
 Ax = xdim/2-90;
@@ -37,19 +37,19 @@ TopDiag = zeros(10,1);
 BottomDiag = ones(5,5);
 
 for i = 1:1:10
-    if (i > 1 && i ~= 10)
-        TopStep(i,i) = 0;
-        TopStep(i-1,i) = 0;
-    elseif(i == 10)
-        TopStep(i-1,i) = 0;
-    else
-        TopStep(i,i) = 0;
-    end
+  if (i > 1 && i ~= 10)
+    TopStep(i,i) = 0;
+    TopStep(i-1,i) = 0;
+  elseif(i == 10)
+    TopStep(i-1,i) = 0;
+  else
+    TopStep(i,i) = 0;
+  end
 end
 BottomStep = TopStep;
 
 for j = 1:1:5
-    BottomDiag(j,j) = 0;
+  BottomDiag(j,j) = 0;
 end
 
 % Font position
@@ -64,54 +64,53 @@ figr = figure('Visible', 'On' );
 colormap('hsv');
 
 for n = 0:1:time_tot
-    t = n*deltat;
+  t = n*deltat;
     
-    % Font    
-    Ez(xsource1, ysource1) = exp(-((t-T).^2)/((std_dev.^2)*.2))* sin(2*pi*30e9*t);
-    Ez(xsource2, ysource2) = exp(-((t-T).^2)/((std_dev.^2)*.2))* sin(2*pi*30e9*t);
-    Ez(xsource3, ysource3) = exp(-((t-T).^2)/((std_dev.^2)*.2))* sin(2*pi*30e9*t);
+  % Font    
+  Ez(xsource1, ysource1) = exp(-((t-T).^2)/((sd.^2)*.2))* sin(2*pi*30e9*t);
+  Ez(xsource2, ysource2) = exp(-((t-T).^2)/((sd.^2)*.2))* sin(2*pi*30e9*t);
+  Ez(xsource3, ysource3) = exp(-((t-T).^2)/((sd.^2)*.2))* sin(2*pi*30e9*t);
    
-    if  (mod(n,2) == 1)
-        pcolor(-log(abs(Ez)+1e-30))
-        shading interp
-        colorbar
-        title({['Nt = ',num2str(n)],['Time: ',num2str(t),' sec.']});
-		hold on;
-        plot ([ysource2, ydim-xsource2+ysource2],[xsource2, ydim],'k','linewidth',1);
-        hold off
-        pause(.005)
-
-    end
+  if (mod(n,2) == 1)
+    pcolor(-log(abs(Ez)+1e-30))
+    shading interp
+    colorbar
+    title({['Nt = ',num2str(n)],['Time: ',num2str(t),' sec.']});
+    hold on;
+    plot ([ysource2, ydim-xsource2+ysource2],[xsource2, ydim],'k','linewidth',1);
+    hold off
     pause(.005)
+  end
+  pause(.005)
     
-    for i = 2:1:xdim-1
-        for j = 2:1:ydim-1
-          Hx(i,j) = Hx(i,j) - (deltat/mu0)*(Ez(i,j)-Ez(i-1,j))/deltay;
-          Hy(i,j) = Hy(i,j) + (deltat/mu0)*(Ez(i,j)-Ez(i,j-1))/deltax;
-        end
+  for i = 2:1:xdim-1
+    for j = 2:1:ydim-1
+      Hx(i,j) = Hx(i,j) - (deltat/mu0)*(Ez(i,j)-Ez(i-1,j))/deltay;
+      Hy(i,j) = Hy(i,j) + (deltat/mu0)*(Ez(i,j)-Ez(i,j-1))/deltax;
     end
+  end
     
-    for i = 1:1:xdim-1
-        for j = 1:1:ydim-1
-            if(i>xdim/2)
-                eps = epsilon1;                 % Media 02
-            elseif (i == xdim/2)
-                eps = (epsilon0 + epsilon1)/2;  % Interface
-            else
-                eps = epsilon0;                 % Media 01
-            end 
-                Ez(i,j) = Ez(i,j) + (deltat/eps)*((Hy(i,j+1)-Hy(i,j))/deltax - (Hx(i+1,j)-Hx(i,j))/deltay);        
-        end
+  for i = 1:1:xdim-1
+    for j = 1:1:ydim-1
+      if(i>xdim/2)
+        eps = epsilon1;                 % Medium 02
+      elseif (i == xdim/2)
+        eps = (epsilon0 + epsilon1)/2;  % Middle Interface
+      else
+        eps = epsilon0;                 % Medium 01
+      end 
+      Ez(i,j) = Ez(i,j) + (deltat/eps)*((Hy(i,j+1)-Hy(i,j))/deltax - (Hx(i+1,j)-Hx(i,j))/deltay);        
     end
+  end
     
-    % Boundary Conditions for horn antenna
-    Ez(Ax:1:(Ax)+4,Ay) = BoxLeftSide;
-    Ez((Ax)-1,(Ay):(Ay)+7) = BoxBottom;
-    Ez((Ax)+5,(Ay):(Ay)+5) = BoxTop;
-    Ez((Ax)+6:(Ax)+14,(Ay)+5:(Ay)+14) = Ez((Ax)+6:(Ax)+14,(Ay)+5:(Ay)+14).*TopStep;
-    Ez((Ax):(Ax)+8,(Ay)+7:(Ay)+16) = Ez((Ax):(Ax)+8,(Ay)+7:(Ay)+16).*BottomStep;
-    Ez((Ax)+15:(Ax)+24,(Ay)+14) = TopDiag;
-    Ez((Ax)+7:-1:(Ax)+3,(Ay)+17:(Ay)+21) = Ez((Ax)+7:-1:(Ax)+3,(Ay)+17:(Ay)+21).*BottomDiag;
+  % Boundary Conditions for horn antenna
+  Ez(Ax:1:(Ax)+4,Ay) = BoxLeftSide;
+  Ez((Ax)-1,(Ay):(Ay)+7) = BoxBottom;
+  Ez((Ax)+5,(Ay):(Ay)+5) = BoxTop;
+  Ez((Ax)+6:(Ax)+14,(Ay)+5:(Ay)+14) = Ez((Ax)+6:(Ax)+14,(Ay)+5:(Ay)+14).*TopStep;
+  Ez((Ax):(Ax)+8,(Ay)+7:(Ay)+16) = Ez((Ax):(Ax)+8,(Ay)+7:(Ay)+16).*BottomStep;
+  Ez((Ax)+15:(Ax)+24,(Ay)+14) = TopDiag;
+  Ez((Ax)+7:-1:(Ax)+3,(Ay)+17:(Ay)+21) = Ez((Ax)+7:-1:(Ax)+3,(Ay)+17:(Ay)+21).*BottomDiag;
     
 end
 pause(.1)
